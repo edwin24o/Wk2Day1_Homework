956a20b276123bae44e041882211d139dde58469
edwin24o <edwinolivares24@gmail.com>
2024-07-24 11:55:16 -0500

    done

--- Grocery_list.py
+++ Grocery_list.py
@@ -3,10 +3,21 @@ def add_item(shopping_list):
     shopping_list.append(item)
     print(f"{item} has been added to the list!")
     
+def remove_item(shopping_list):
+    item = input("What would you like to remove from the list? ")
+    if item in shopping_list:
+      shopping_list.remove(item)
+      print(f"'{item}' has been removed from the shopping list.")
+    else:
+        print(f"WAIT! '{item}' is not in the shopping list")
 
-
-
-
+def print_list(shopping_list):
+    if shopping_list:
+        print("Shopping list:")
+        for item in shopping_list:
+            print(f"- {item}")
+    else:
+        print("Your shopping list is EMPTY")
 
 def main():
     shopping_list = []
@@ -23,11 +34,11 @@ Shopping List Helper:
         if action == "1":  
             add_item(shopping_list) 
         elif action == "2": 
-            subtraction()
+            remove_item(shopping_list)
         elif action == "3":
-            division()
+            print_list(shopping_list)
         elif action == "4":
-            multiply()
+            print('GOODBYE!!')
             break
         else:
             print("ERROR PLEASE SELECT WHAT IS DISPLAYED")
